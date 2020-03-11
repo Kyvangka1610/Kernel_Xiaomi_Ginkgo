f829347bdf56ee26402c52ebe34656d956f22a00
Kyvangka1610 <kyvangka2002@gmail.com>
2020-03-11 12:36:42 +0700

    Bring Up SixTeen kernel
    Signed-off-by: Kyvangka1610 <kyvangka2002@gmail.com>

---
+++ clang.sh
@@ -0,0 +1,10 @@
+export KBUILD_BUILD_USER="elang"
+export KBUILD_BUILD_HOST="kyvangkaelang"
+export ARCH=arm64
+export SUBARCH=arm64
+make O=out vendor/chips_defconfig
+make -j$(nproc --all) O=out ARCH=arm64 \
+                      CC="/home/kyvangka1610/kernel/clang-11/bin/clang" \
+                      CLANG_TRIPLE="aarch64-linux-gnu-" \
+                      CROSS_COMPILE="/home/kyvangka1610/kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+
