995df9e8408347f6fd1567ff665d91a9ad5dcaaf
Kyvangka1610 <kyvangka2002@gmail.com>
2020-03-22 21:09:45 +0700

    Keenel Update : Fix Error Build
    Signed-off-by: Kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ make -j$(nproc --all) O=out ARCH=arm64 \
                       CC="/home/kyvangka1610/kernel/Clang-10/bin/clang" \
                       CLANG_TRIPLE="aarch64-linux-gnu-" \
                       CROSS_COMPILE="/home/kyvangka1610/kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android-" \
-		      CROSS_COMPILE_ARM32="/home/kyvangka1610/kernel/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-" 
+		      CROSS_COMPILE_ARM32="/home/kyvangka1610/kernel/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
